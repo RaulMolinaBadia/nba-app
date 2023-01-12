6b83a28506b8b1f97585eb23f36ce9945a6ec42f
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-12 11:39:18 +0100

    footer done

---
+++ components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterContainer } from './styles'
+
+const Footer = () => {
+  return (
+    <footer>
+      <FooterContainer>a</FooterContainer>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Footer/styles.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const FooterContainer = styled.footer`
+  background-color: #fff3;
+  position: fixed;
+  bottom: 0;
+  width: 100vw;
+  height: 50px;
+`
