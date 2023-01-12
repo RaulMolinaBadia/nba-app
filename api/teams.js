f9a82f99b3d848f2b0783cd8742896ae1d461cb3
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-12 09:58:42 +0100

    fix

--- api/teams.js
+++ api/teams.js
@@ -2,11 +2,14 @@
 
 import axios from "axios";
 
-const url = "https://www.balldontlie.io/api/v1/teams";
-export default function handler(req, res) {
+const url = "https://www.balldontlie.io/api/v1/teams/14";
+export default function handler(res,req) {
     // res.status(200).json({ name: "John Doe" });
     axios.get(url).then((response) => {
-        res.status(200).json(response.data);
+        // url.status(200).json(response.data);
         console.log(response.data);
     });
+    <div>
+        <h1>hello</h1>
+    </div>
 }
