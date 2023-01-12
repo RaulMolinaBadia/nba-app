facafae4ac7d57f4a5a9439d441c746089651267
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-12 09:12:21 +0100

    example api teams

---
+++ pages/api/teams.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import axios from "axios";
+
+const url = "https://www.balldontlie.io/api/v1/teams";
+export default function handler(req, res) {
+    // res.status(200).json({ name: "John Doe" });
+    axios.get(url).then((response) => {
+        res.status(200).json(response.data);
+        console.log(response.data);
+    });
+}
