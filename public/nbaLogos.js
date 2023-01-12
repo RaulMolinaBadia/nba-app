2aca78169ed0e48313ee0f3f9a434cd9ce499ec6
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2023-01-12 12:08:22 +0100

    nbaLogos list added

---
+++ public/nbaLogos.js
@@ -0,0 +1,66 @@
+import logo_76ers from './76ers-logo.png'
+import logo_blazers from './blazers-logo.png'
+import logo_bucks from './bucks-logo.png'
+import logo_bulls from './bulls-logo.png'
+import logo_cavaliers from './cavaliers-logo.png'
+import logo_celtics from './celtics-logo.png'
+import logo_clippers from './clippers-logo.png'
+import logo_grizzlies from './grizzlies-logo.png'
+import logo_hawks from './hawks-logo.png'
+import logo_heat from './heat-logo.png'
+import logo_hornets from 'hornets-logo.png'
+import logo_jazz from './jazz-logo.png'
+import logo_kings from './kings-logo.png'
+import logo_knicks from './knicks-logo.png'
+import logo_lakers from './lakers-logo.png'
+import logo_magic from './magic-logo.png'
+import logo_mavericks from './mavericks-logo.png'
+import logo_nets from './nets-logo.png'
+import logo_nuggets from './nuggets-logo.png'
+import logo_pacers from './pacers-logo.png'
+import logo_pelicans from './pelicans-logo.png'
+import logo_pistons from './pistons-logo.png'
+import logo_raptors from './raptors-logo.png'
+import logo_rockets from './rockets-logo.png'
+import logo_spurs from './spurs-logo.png'
+import logo_suns from './suns-logo.png'
+import logo_thunder from './thunder-logo.png'
+import logo_timberwolves from './timberwolves-logo.png'
+import logo_warriors from './warriors-logo.png'
+import logo_wizards from './wizards-logo.png'
+
+
+const teamsList = [
+    { team: "76ers", logo: logo_76ers },
+    { team: "Blazers", logo: logo_blazers },
+    { team: "Bucks", logo: logo_bucks },
+    { team: "Bulls", logo: logo_bulls },
+    { team: "Cavaliers", logo: logo_cavaliers },
+    { team: "Celtics", logo: logo_celtics },
+    { team: "Clippers", logo: logo_clippers },
+    { team: "Grizzlies", logo: logo_grizzlies },
+    { team: "Hawks", logo: logo_hawks },
+    { team: "Heat", logo: logo_heat },
+    { team: "Hornets", logo: logo_hornets },
+    { team: "Jazz", logo: logo_jazz },
+    { team: "Kings", logo: logo_kings },
+    { team: "Knicks", logo: logo_knicks },
+    { team: "Lakers", logo: logo_lakers },
+    { team: "Magic", logo: logo_magic },
+    { team: "Mavericks", logo: logo_mavericks },
+    { team: "Nets", logo: logo_nets },
+    { team: "Nuggets", logo: logo_nuggets },
+    { team: "Pacers", logo: logo_pacers },
+    { team: "Pelicans", logo: logo_pelicans },
+    { team: "Pistons", logo: logo_pistons },
+    { team: "Raptors", logo: logo_raptors },
+    { team: "Rockets", logo: logo_rockets },
+    { team: "Spurs", logo: logo_spurs },
+    { team: "Suns", logo: logo_suns },
+    { team: "Thunder", logo: logo_thunder },
+    { team: "Timberwolves", logo: logo_timberwolves },
+    { team: "Warriors", logo: logo_warriors },
+    { team: "Wizards", logo: logo_wizards }
+];
+
+export default teamsList;
