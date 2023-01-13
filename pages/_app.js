b34e2aa5463856ddae0aadc54fa384609494dd2b
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-13 15:50:06 +0100

    remove imports app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,4 @@
 import '../styles/globals.css'
-import { NavBar } from '../styles/pages/LandingPage'
-import MenuBar from '../components/MenuBar'
-import Image from 'next/image'
 
 function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
