4bfba57b293183424bbc67e71dcfb96793cb64ab
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-14 17:51:19 +0100

    fix access home Link

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import MenuBar from '../components/MenuBar/index'
-import { NavBar } from '../styles/LandingPage'
+import { NavBar } from '../styles/pages/LandingPage'
 import TeamListBar from '../components/TeamListBar'
+import Link from 'next/link'
 
 export default function Home () {
   return (
@@ -16,7 +17,9 @@ export default function Home () {
         />
       </Head>
       <NavBar>
-        <Image src='/app-logo/Logo-NBA.png' width={100} height={57} alt='logoNBA' />
+        <Link href='/'>
+          <Image src='/app-logo/Logo-NBA.png' width={100} height={57} alt='logoNBA' />
+        </Link>
         <MenuBar teamName='Bulls' />
       </NavBar>
       <TeamListBar />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import MenuBar from '../components/MenuBar/index'
 import { NavBar } from '../styles/pages/LandingPage'
 
@@ -15,7 +16,9 @@ export default function LandingPage () {
         />
       </Head>
       <NavBar>
-        <Image src='/app-logo/Logo-NBA.png' width={100} height={57} alt='logoNBA' />
+        <Link href='/home'>
+          <Image src='/app-logo/Logo-NBA.png' width={100} height={57} alt='logoNBA' />
+        </Link>
         <MenuBar teamName='Bulls' />
       </NavBar>
     </div>
