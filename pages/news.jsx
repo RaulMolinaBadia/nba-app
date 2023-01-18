910cac5ede437662ab85a98c65250e6a7f34f611
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-18 10:08:18 +0100

    fix

--- pages/news.jsx
+++ pages/news.jsx
@@ -28,44 +28,7 @@ export default function News (props) {
       <TeamListBar />
       <Frame
         teamName='NEWS'
-        news={props.news}
-        imagesUrls={props.imagesUrls}
       />
     </div>
   )
-}
-
-async function getImageUrl (url) {
-  const { data } = await axios.get(url)
-  const $ = cheerio.load(data)
-  return $('.ArticleContent_article__NBhQ8 img').attr('src')
-}
-
-export async function getStaticProps () {
-  const headersList = {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-    'X-RapidAPI-Key': 'e8079d9059mshaf2885320dead3bp16650ejsne54672d3a921'
-  }
-  const response = await fetch('https://nba-latest-news.p.rapidapi.com/articles?source=nba', {
-    method: 'GET',
-    headers: headersList
-  })
-  const news = await response.json()
-
-  const imagesUrls = []
-  const defaultImageUrl = 'defaultImage.jpg'
-  for (const newsItem of news) {
-    const imageUrl = await getImageUrl(newsItem.url)
-    if (imageUrl) {
-      imagesUrls.push(imageUrl)
-    } else {
-      imagesUrls.push(defaultImageUrl)
-    }
-  }
-
-  return {
-    props: { news, imagesUrls },
-    revalidate: 10 // rerun after 10 seconds
-  }
 }
--- pages/players/[id].jsx
+++ pages/players/[id].jsx
@@ -5,12 +5,10 @@ import { NavBar } from '../../styles/pages/LandingPage'
 import MenuBar from '../../components/MenuBar/index'
 import TeamListBar from '../../components/TeamListBar'
 import Frame from '../../components/Frame'
-import TeamsList from '../../public/nbaLogos'
 
 export default function DynamicPage ({ id }) {
   const router = useRouter()
   const { query } = router
-  const findTeam = TeamsList.find(team => team.name === query.id)
   return (
     <div>
       <h1 />
