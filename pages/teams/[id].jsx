ca0a77add8bb805c155f8c0f938c65f39d57efd5
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-18 10:21:06 +0100

    fix

--- pages/teams/[id].jsx
+++ pages/teams/[id].jsx
@@ -35,38 +35,3 @@ export default function DynamicPage ({ id }) {
 DynamicPage.getInitialProps = async ({ query }) => {
   return { id: query.id }
 }
-
-async function getImageUrl (url) {
-  const { data } = await axios.get(url)
-  const $ = cheerio.load(data)
-  return $('.ArticleContent_article__NBhQ8 img').attr('src')
-}
-
-export async function getStaticProps () {
-  const headersList = {
-    Accept: '*/*',
-    'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-    'X-RapidAPI-Key': 'e8079d9059mshaf2885320dead3bp16650ejsne54672d3a921'
-  }
-  const response = await fetch('https://nba-latest-news.p.rapidapi.com/articles?source=nba', {
-    method: 'GET',
-    headers: headersList
-  })
-  const news = await response.json()
-
-  const imagesUrls = []
-  const defaultImageUrl = 'defaultImage.jpg'
-  for (const newsItem of news) {
-    const imageUrl = await getImageUrl(newsItem.url)
-    if (imageUrl) {
-      imagesUrls.push(imageUrl)
-    } else {
-      imagesUrls.push(defaultImageUrl)
-    }
-  }
-
-  return {
-    props: { news, imagesUrls },
-    revalidate: 10 // rerun after 10 seconds
-  }
-}
