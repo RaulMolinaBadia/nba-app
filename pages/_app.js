03c9d2f7e8a2dd773ebc4cdb3e3b9d9bcc181b51
Yerai Rodriguez Mancilla <yerai.rodriguez.7e5@itb.cat>
2023-01-26 14:43:21 +0100

    loader wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
 
+function Loading () {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true)
+    const handleComplete = (url) => (url !== router.asPath) && setTimeout(() => { setLoading(false) }, 5000)
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routerChangeComplete', handleComplete)
+    router.events.on('routerChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routerChangeComplete', handleComplete)
+      router.events.off('routerChangeError', handleComplete)
+    }
+  })
+  return loading && <div>loading...</div>
+}
+
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<><Loading /><Component {...pageProps} /></>)
 }
 
 export default MyApp
