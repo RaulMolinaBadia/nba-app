f9ab0028e9b1c0057ba4565999d7da7dbe560cd5
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-26 12:02:43 +0100

    footer test done and all test envirorment

---
+++ __test__/footer.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Footer from '../components/Footer/index'
+
+describe('Footer', () => {
+  it('renders the footer', () => {
+    render(<Footer />)
+
+    const footerText = screen.getByTestId('footerText')
+
+    expect(footerText).toBeInTheDocument()
+  })
+})
