0772582243c459d98d09b331ca2f9eba976479c2
Yerai Rodriguez <yerai.rodriguez.7e5@itb.cat>
2023-01-27 11:58:20 +0100

    loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,28 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '../styles/globals.css'
-import React from 'react'
-import Router from 'next/router'
-import LoadingPage from '../components/LoadingPage'
+
+function Loading () {
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const handleStart = (url) => (url !== router.asPath) && setLoading(true)
+    const handleComplete = (url) => (url !== router.asPath) && setTimeout(() => { setLoading(false) }, 5000)
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routerChangeComplete', handleComplete)
+    router.events.on('routerChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routerChangeComplete', handleComplete)
+      router.events.off('routerChangeError', handleComplete)
+    }
+  })
+  return loading && <div>loading...</div>
+}
 
 function MyApp ({ Component, pageProps }) {
-  const [loading, setLoading] = React.useState(false)
-  React.useEffect(() => {
-    const start = () => {
-      console.log('start')
-      setLoading(true)
-    }
-    const end = () => {
-      console.log('finished')
-      setLoading(false)
-    }
-    Router.events.on('routeChangeStart', start)
-    Router.events.on('routeChangeComplete', end)
-    Router.events.on('routeChangeError', end)
-    return () => {
-      Router.events.off('routeChangeStart', start)
-      Router.events.off('routeChangeComplete', end)
-      Router.events.off('routeChangeError', end)
-    }
-  }, [])
-  return (
-    <>
-      {loading
-        ? (
-          <LoadingPage />
-          )
-        : (
-          <Component {...pageProps} />
-          )}
-    </>
-  )
+  return (<><Loading /><Component {...pageProps} /></>)
 }
+
 export default MyApp
