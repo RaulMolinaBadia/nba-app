d2a3576046f56a9b8552b5acbd56182fa1192283
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-27 11:53:14 +0100

    fix

--- __test__/components/footer.test.jsx
+++ __test__/components/footer.test.jsx
@@ -1,12 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import Footer from '../../components/Footer/index'
 
-describe('Footer', () => {
   it('renders the footer', () => {
     render(<Footer />)
-
     const footerText = screen.getByTestId('footerText')
-
     expect(footerText).toBeInTheDocument()
   })
-})
