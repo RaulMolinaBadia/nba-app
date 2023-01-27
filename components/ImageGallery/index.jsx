a0ad3f96cac0fc89204ef8335e35d5e4844f5abe
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-27 13:48:09 +0100

    create

---
+++ components/ImageGallery/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ImageGallery = () => {
+  return (
+    <section>
+      <h1>Image Gallery</h1>
+      <img src='' alt='' />
+      <img src='' alt='' />
+      <img src='' alt='' />
+      <img src='' alt='' />
+      <img src='' alt='' />
+    </section>
+  )
+}
+
+export default ImageGallery
